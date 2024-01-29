31db2709d849c20f6152c45e5401a1854653a5bd
Max Murakami-Moses <max.murakamimoses24@gmail.com>
2024-01-29 15:31:22 -0500

    Changed Tab Name

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.svg'
+import logo from '../assets/Logo.svg'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput';
 
